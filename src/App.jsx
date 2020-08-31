bcfad34b3c96e177abcfd948904f3aadfabedd83
Karnak19 <basile64.v@gmail.com>
2020-08-31 16:08:48 +0200

    init template :fire:

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+// import logo from './reactlogo.png';
+
+import './App.css';
+
+function App() {
+  return (
+    <main className="container">
+      <div className="item">{/* <img src={logo} alt="React Logo" /> */}</div>
+      <div className="item">
+        <h1>Hello world !</h1>
+      </div>
+      <div className="item">
+        <p>Welcome to your fresh, lightweight, React App ! &#127752;</p>
+      </div>
+      <div className="item">
+        <p>
+          Start in the <code>App.jsx</code> component !
+        </p>
+      </div>
+    </main>
+  );
+}
+
+export default App;
